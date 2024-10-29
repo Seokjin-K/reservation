89de971bdd7af892b623f2ed6ece4f12ae02a79b
seokjin <wlsdldos00@gmail.com>
2024-10-30 01:36:47 +0900

    Feat: Create AlreadyExistAccountException extentd to AbstractException

---
+++ common/src/main/java/com/reservation/exception/extend/AlreadyExistAccountException.java
@@ -0,0 +1,16 @@
+package com.reservation.exception.extend;
+
+import com.reservation.exception.AbstractException;
+import org.springframework.http.HttpStatus;
+
+public class AlreadyExistAccount extends AbstractException {
+    @Override
+    public int getStatusCode() {
+        return HttpStatus.BAD_REQUEST.value();
+    }
+
+    @Override
+    public String getMessage() {
+        return "이미 존재하는 계정명입니다.";
+    }
+}
