ef5c93e75037b770faa831f200773e16f3446629
seokjin <wlsdldos00@gmail.com>
2024-11-03 14:59:02 +0900

    Refactor: Modify annotation @NotBlank to @NotNull

--- dto/src/main/java/com/reservation/store/StoreRequest.java
+++ dto/src/main/java/com/reservation/store/StoreRequest.java
@@ -2,16 +2,16 @@ package com.reservation.store;
 
 import lombok.Getter;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Getter
 public class StoreRequest {
 
-    @NotBlank(message = "매장 이름은 필수입니다.")
+    @NotNull(message = "매장 이름은 필수입니다.")
     private String name;
 
-    @NotBlank(message = "매장 주소는 필수입니다.")
+    @NotNull(message = "매장 주소는 필수입니다.")
     private String address;
 
     @Size(max = 1000, message = "매장 설명은 1000자를 초과할 수 없습니다.")
