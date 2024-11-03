a8bd9dec3a912f5b56350ddca85677e64fccf31f
seokjin <wlsdldos00@gmail.com>
2024-11-03 20:52:25 +0900

    Feat: Add findByStoreEntity_IdOrderByCreate_AtDesc
    - 날짜 순으로 내림차순 정렬하여 해당 storeId의 리뷰를 모두 반환

--- domain/src/main/java/com/reservation/repository/review/ReviewRepository.java
+++ domain/src/main/java/com/reservation/repository/review/ReviewRepository.java
@@ -1,17 +1,27 @@
 package com.reservation.repository.review;
 
 import com.reservation.entity.review.ReviewEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.domain.Page;
 
 
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
 
     boolean existsByReservationEntity_Id(Long reservationId);
 
-    // JPQL
-    @Query(value = "SELECT COALESCE(AVG(rating), 0) FROM review WHERE store_id = :storeId",
+    @Query(value = "SELECT COALESCE(AVG(rating), 0) " +
+            "FROM review " +
+            "WHERE store_id = :storeId",
             nativeQuery = true)
     Double calculateStoreAverageRating(@Param("storeId") Long storeId);
+
+    @Query(value = "SELECT * FROM Review r " +
+            "WHERE r.store_id = :storeId " +
+            "ORDER BY r.created_at DESC",
+            nativeQuery = true)
+    Page<ReviewEntity> findByStoreEntity_IdOrderByCreated_AtDesc(
+            @Param("storeId") Long storeId, Pageable pageable);
 }
