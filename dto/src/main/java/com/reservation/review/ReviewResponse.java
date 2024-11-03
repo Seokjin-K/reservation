695319c66e24abe4c19d7787dd384d3c25221ab7
seokjin <wlsdldos00@gmail.com>
2024-11-03 20:53:06 +0900

    Feat: Add createdAt and updatedAt in ReviewResponse

--- dto/src/main/java/com/reservation/review/ReviewResponse.java
+++ dto/src/main/java/com/reservation/review/ReviewResponse.java
@@ -4,18 +4,24 @@ import com.reservation.entity.review.ReviewEntity;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 public class ReviewResponse {
     private Long reviewId;
     private String content;
     private Double rating;
+    private LocalDateTime createAt;
+    private LocalDateTime updatedAt;
 
     public static ReviewResponse from(ReviewEntity reviewEntity) {
         return ReviewResponse.builder()
                 .reviewId(reviewEntity.getId())
                 .content(reviewEntity.getContent())
                 .rating(reviewEntity.getRating())
+                .createAt(reviewEntity.getCreatedAt())
+                .updatedAt(reviewEntity.getUpdatedAt())
                 .build();
     }
 }
